477b7cc804abc81659c007f12d4fe866af7bf42f
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-11-22 11:33:41 +0630

    bug fixed and style update

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -5,14 +5,12 @@ import { useState } from "react";
 let ExpenseItem = ({ title, amount, date }) => {
   let [new_title, setTitle] = useState(title);
 
-  let logConsole = () => setTitle("update title"); //setTitle will call the useState again and  update the state and rerender the component
   return (
     <div className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{new_title}</h2>
-        <div className="item__price">$ {amount}</div>
-        <button onClick={logConsole}>change title</button>
+        <div className="expense-item__price">$ {amount}</div>
       </div>
     </div>
   );
